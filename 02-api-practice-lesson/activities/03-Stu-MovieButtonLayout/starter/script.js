4bc468222ff10a7df7e774759f6c30d264420960
Adrian Tut <adriantutuianu01@gmail.com>
2024-01-10 19:52:14 +0000

    Movie button layout task

--- 02-api-practice-lesson/activities/03-Stu-MovieButtonLayout/starter/script.js
+++ 02-api-practice-lesson/activities/03-Stu-MovieButtonLayout/starter/script.js
@@ -3,16 +3,21 @@ const movies = ["The Matrix", "Dune", "Mr. Right", "The Lion King"];
 
 // Function for displaying movie data
 function renderButtons() {
-
   // YOUR CODE GOES HERE
-
+  movies.forEach(function (movie) {
+    var targetBtnContainer = $("#buttons-view");
+    var movieBtn = $("<button>");
+    movieBtn.text(movie);
+    targetBtnContainer.append(movieBtn);
+  });
 }
 
 // This function handles events where one button is clicked
-$("#add-movie").on("click", function () {
-
+$("#add-movie").on("click", function (event) {
   // YOUR CODE GOES HERE
-
+  event.preventDefault();
+  var newMovie = $("#movie-input").val();
+  console.log(newMovie);
 });
 
 // Calling the renderButtons function to display the initial list of movies
