e1b233243e9d4d1e3c8b6836b40cc258a83fad20
Adrian Tut <adriantutuianu01@gmail.com>
2024-01-11 00:38:26 +0000

    BandsinTown app solved

--- 02-api-practice-lesson/activities/06-BandsInTownApp/starter/script.js
+++ 02-api-practice-lesson/activities/06-BandsInTownApp/starter/script.js
@@ -1,14 +1,43 @@
 function searchBandsInTown(artist) {
+  let url = `https://rest.bandsintown.com/artists/${artist}?app_id=12`;
+  console.log(url);
   // Add code to query the bands in town api searching for the artist received as an argument to this function
   // Using jQuery, append the following to the #artist-div :
-  // The artist's name
-  console.log(artist);
-  $("#artist-div");
-  // The artists thumbnail image
-  // The number of fans tracking this artist
-  // The number of upcoming events for this artist
-  // A link to the bandsintown url for this artist
-  // Note: Append actual HTML elements, not just text
+  fetch(url)
+    .then(function (resp) {
+      return resp.json();
+    })
+    .then(function (data) {
+      const artistDiv = $("#artist-div");
+
+      // The artist's name
+      const artistName = data.name;
+      const pOne = $("<h1>").text("Artist Name: " + artistName);
+      artistDiv.append(pOne);
+      // The artists thumbnail image
+      const thumbnail = data.image_url;
+      const imageThumbnail = $("<img>").attr("src", thumbnail);
+      artistDiv.append(imageThumbnail);
+      // The number of fans tracking this artist
+      const fans = data.tracker_count;
+      const pTwo = $("<p>").text("Artist fans: " + fans);
+      artistDiv.append(pTwo);
+      // The number of upcoming events for this artist
+      const upcomingEvents = data.upcoming_event_count;
+      const pThree = $("<p>").text("Artist upcoming events: " + upcomingEvents);
+      artistDiv.append(pThree);
+      // A link to the bandsintown url for this artist
+      const linkBnasintown = data.url;
+      console.log(linkBnasintown);
+      const linkOne = $("<a>")
+        .attr("href", linkBnasintown)
+        .text("See Tour Dates");
+      artistDiv.append(linkOne);
+      // Note: Append actual HTML elements, not just text
+      // Empty the contents of the artist-div, append the new artist content
+      $("#artist-div").empty();
+      $("#artist-div").append(pOne, imageThumbnail, pTwo, pThree, linkOne);
+    });
 }
 
 // Event handler for user clicking the select-artist button
@@ -23,6 +52,3 @@ $("#select-artist").on("click", function (event) {
 });
 
 // const queryURL = `377fcf58-190d-42b2-9796-b6ea86442f08`;
-
-var queryString = "https://rest.bandsintown.com/artists/{maroon+5}";
-console.log(queryString);
